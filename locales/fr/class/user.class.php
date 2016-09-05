d54810cb537438ab5aad76a74c33715a558bac59
LordTama <mickael.moreira@gmail.com>
2016-09-05 02:07:39 +0200

    Local class user

---
+++ locales/fr/class/user.class.php
@@ -0,0 +1,18 @@
+<?php
+$lang = array(
+	'invalid_user_pass'		=>	'Le nom de l\'utilisateur et/ou le mot de passe est incorrect.',
+	'inactif_user'			=>	'Votre compte a été desactivé. Veuillez contacter l\'administrateur.',
+
+	# Check Info
+	# ----------
+	'invalid_usr_login'		=>	'Le login est incorrect.',
+	'invalid_usr_pass'		=>	'Le mot de passe doit être hashé en MD5.',
+	'invalid_usr_etat'		=>	'L\'état de l\'utilisateur est incorrect.',
+	'usr_pass_empty' => '- Veuillez renseigner un mot de passe',
+	'usr_name_empty' => '- Veuillez renseigner un nom',
+	'usr_first_name_empty' => '- Veuillez renseigner un prénom',
+	'usr_mail_empty' => '- Veuillez renseigner une adresse mail',
+	'usr_init_empty' => '- Veuillez renseigner les initiales',
+	'usr_login_empty' => '- Veuillez renseigner le login utilisateur'
+);
+?>
